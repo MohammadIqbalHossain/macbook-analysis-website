f6726fa5dd42f0eee440be4d9b71724e16f82292
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-04-04 16:51:10 +0600

    404 page added

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () => {
+    return (
+        <div className="container">
+            <h1 className="fancy-oops">Oops!</h1>
+            <h1 className="text-bold text-3xl font-semibold text-green-900">404-PAGE NOT FOUND</h1>
+            <p className="text-xl font-serif mt-4">The page you're looking for might have been removed had it's name changed or it't temporary unavilable.</p>
+            <button className="p-3 px-6 bg-blue-600 mt-4 rounded-full text-lg font-bold text-white font-mono mb-5">
+                <a href="/">GO TO HOME PAGE</a>
+            </button>
+        </div>
+    );
+};
+
+export default NotFound;
